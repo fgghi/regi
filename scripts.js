98e12a784daefdb2eb98ce680325c757c704eba8
Sergio <sergio.bustamante@ucb.edu.bo>
2023-12-07 13:01:01 -0400

    Añadido detalles finales

--- scripts.js
+++ scripts.js
@@ -1,22 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const images = document.querySelectorAll('.animated-image');
-    let delay = 0;
-  
-    images.forEach(function (image) {
-      setTimeout(function () {
-        image.style.display = 'block'; // Mostrar la imagen
-      }, delay);
-      delay += 1500; // Incrementar el retardo para la siguiente imagen
-    });
-  
-    // Preguntar al final de las imágenes
-    setTimeout(function () {
-      const confirm = window.confirm('Regina Victoria Parra, ¿quieres ser mi novia?');
-      if (confirm) {
-        alert('💞 ¡Felicidades! 💞');
-      } else {
-        alert('😔 ¡Qué pena! 😔');
-      }
-    }, delay);
-  });
-  
+
+const contentSection = document.querySelector('.content');
+function checkVisible() {
+  const contentPosition = contentSection.getBoundingClientRect();
+  if (contentPosition.top < window.innerHeight && contentPosition.bottom >= 0) {
+    contentSection.classList.add('visible');
+    contentSection.classList.remove('hidden');
+  } else {
+    contentSection.classList.add('hidden');
+    contentSection.classList.remove('visible');
+  }
+}
+window.addEventListener('scroll', checkVisible);
+window.addEventListener('load', checkVisible);
+const btnIniciar = document.getElementById('btnIniciar');
+const botones = document.getElementById('botones');
+const btnSi = document.getElementById('btnSi');
+const btnNo = document.getElementById('btnNo');
+const mensaje = document.getElementById('mensaje');
+const imagenNo = document.getElementById('imagenNo');
+const audio = document.getElementById('audio');
+
+btnIniciar.addEventListener('click', function() {
+  botones.style.display = 'block';
+});
+
+btnSi.addEventListener('click', function() {
+  mensaje.style.display = 'block';
+  audio.play();
+});
+
+btnNo.addEventListener('click', function() {
+  imagenNo.style.display = 'block';
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var miAudio = document.getElementById("myAudio");
+  miAudio.volume = 0.3; // Establecer el volumen predeterminado a 0.3 (puedes ajustar este valor)
+});
+
+function bajarVolumen() {
+  var miAudio = document.getElementById("myAudio");
+  if (miAudio.volume > 0) {
+      miAudio.volume -= 0.1; // Reducir el volumen en 0.1 (puedes cambiar este valor)
+  }
+}
